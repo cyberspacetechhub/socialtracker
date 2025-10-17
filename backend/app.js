e1655ed38f64029abe203283756035e0719e871b
Cyberspace Tech Hub <esomchi8117@gmail.com>
2025-10-17 18:42:50 +0100

    changes commit

--- backend/app.js
+++ backend/app.js
@@ -33,9 +33,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin || '*');
   }
   
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  // res.header('Access-Control-Allow-Credentials', 'true');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5000',
-    'http://127.0.0.1:3000', 
+    'http://127.0.0.1:3000',
     'https://my-social-tracker.vercel.app',
+    'https://https://socialtracker-nlnk.onrender.com',
     'http://localhost:5173',
     'http://localhost:5174',
     /^chrome-extension:\/\//
